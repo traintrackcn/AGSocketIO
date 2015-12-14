de7551df3639e381a17b36cde82cdc895064400d
Tao Yunfei <traintrackcn@gmail.com>
2015-12-14 19:32:41 +0800

    init

---
+++ AGSocketIO/AGSocketIO.h
@@ -0,0 +1,19 @@
+//
+//  AGSocketIO.h
+//  AGSocketIO
+//
+//  Created by Tao Yunfei on 12/14/15.
+//  Copyright © 2015 AboveGEM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AGSocketIO.
+FOUNDATION_EXPORT double AGSocketIOVersionNumber;
+
+//! Project version string for AGSocketIO.
+FOUNDATION_EXPORT const unsigned char AGSocketIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AGSocketIO/PublicHeader.h>
+
+
